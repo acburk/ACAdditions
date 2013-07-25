91c4fdf1aa762e028e5572c5b58e6cc717d0a4be
acburk <adam.burkepile@gmail.com>
2013-07-25 11:21:59 -0700

    added new error alerts

---
+++ ACAdditions/NSError+ACAdditions.h
@@ -0,0 +1,14 @@
+//
+//  NSError+ACAdditions.h
+//  ACAdditions
+//
+//  Created by Adam Burkepile on 7/25/13.
+//  Copyright (c) 2013 Adam Burkepile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSError (ACAdditions)
+- (void)displayFullscreenShadeAlert;
+- (void)displayAlert;
+@end
